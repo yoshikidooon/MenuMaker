13f62c40fe4c7979f6c93236f6b28d2ff5f70e31
yoshikidooon <yoshiki19990408@docomo.ne.jp>
2020-05-16 21:16:47 +0900

    Initial Commit

---
+++ MenuMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuMaker
+//
+//  Created by 藤田佳己 on 2020/05/16.
+//  Copyright © 2020 Fujita Fujimon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
